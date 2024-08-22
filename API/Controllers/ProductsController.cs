fb8ab2ce852c417df1cada073044d9ce843435ec
Boyko Nikolaev <boykonikolaev1997@gmail.com>
2024-08-22 17:47:37 +0300

    filtering and sorting in Product repo and controller

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -11,9 +11,9 @@ namespace API.Controllers;
 public class ProductsController(IProductRepository repo) : ControllerBase
 {	
 	[HttpGet]
-	public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+	public async Task<ActionResult<IEnumerable<Product>>> GetProducts(string? brand, string? type, string? sort)
 	{
-		return Ok(await repo.GetProductsAsync());
+		return Ok(await repo.GetProductsAsync(brand, type, sort));
 	}
 	
 	[HttpGet("{id:int}")]
@@ -69,5 +69,17 @@ public class ProductsController(IProductRepository repo) : ControllerBase
 		}
 		
 		return BadRequest("Problem deleting the product");
+	}
+	
+	[HttpGet("brands")]
+	public async Task<ActionResult<IReadOnlyList<string>>> GetBrands()
+	{
+		return Ok(await repo.GetBrandsAsync());
+	}
+	
+	[HttpGet("types")]
+	public async Task<ActionResult<IReadOnlyList<string>>> GetTypes()
+	{
+		return Ok(await repo.GetTypesAsync());
 	}
 }
